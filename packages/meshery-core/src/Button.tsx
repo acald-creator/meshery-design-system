551d999bd62e9cfca74da1039db1088e5a770e53
Antonette Caldwell <pullmana8@gmail.com>
2023-03-03 21:24:55 -0600

    feat(workspace): installed mui v5
    Configured @meshery/core and updated the existing
    Button with MuiButton.
    Update Button story from @docs to ensure that the
    new button change is reflected.

--- packages/meshery-core/src/Button.tsx
+++ packages/meshery-core/src/Button.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
+import { Button as MuiButton } from "@mui/material";
 
 export interface ButtonProps {
   children: React.ReactNode;
 }
 
 export function Button(props: ButtonProps) {
-  return <button>{props.children}</button>;
+  return <MuiButton variant="contained">{props.children}</MuiButton>;
 }
 
 Button.displayName = "Button";
