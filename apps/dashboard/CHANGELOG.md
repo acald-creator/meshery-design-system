f6a094a5701c7185438179e1b5e9a7a2b44848cf
Antonette Caldwell <pullmana8@gmail.com>
2023-03-06 16:38:46 -0600

    chore(dashboard): updated dashboard's deps and configs
    Modified next.config.js to transpile packages from @meshery/core
    Moved some of the deps to devDeps
    Include some of the workspace packages: tsconfig and eslint-config-meshery
    Modified tsconfig to use the nextjs.json from eslint-config.meshery
    Formatted files

---
+++ apps/dashboard/CHANGELOG.md
@@ -0,0 +1,9 @@
+# dashboard
+
+## 0.1.1
+
+### Patch Changes
+
+- Updated dependencies [1e0f995]
+- Updated dependencies [9d05bca]
+  - @meshery/core@0.1.0
--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -1,6 +1,14 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  transpilePackages: ["@meshery/core"],
+  output: "standalone",
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  }
 }
 
 module.exports = nextConfig
